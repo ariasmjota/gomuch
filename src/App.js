db1ad5567fdbc4f32837acb112a029755a23fb50
Maria Arias <majobel10@hotmail.com>
2020-10-22 09:25:27 -0500

    Css+html done

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
           </p>
           <div className="banner__text-input">
             <p>Prueba goMunch gratis.</p>
-            <input type="text" className="inputText"></input>
+            <input type="text" className="inputText" placeholder="Correo electrónico"></input>
             <button className="button">Probar ahora</button>
           </div>
         </article>
@@ -83,11 +83,65 @@ function App() {
         </section>
       </section>
       <section className="gomunch">
-
+        <article className="gomunch__top">
+          <div className="gomunch__top-left">
+            <img
+              src={process.env.PUBLIC_URL + "/assets/img/mock2.png"}
+              alt="Relación a distancia"
+            ></img>
+          </div>
+          <div className="gomunch__top-right">
+            <h2 className="values__title">
+              Restaurantes a un tap de distancia
+            </h2>
+            <p>
+              Encuentra restaurantes en la ciudad que vive tu persona especial
+              de una manera sencilla y conﬁable.
+            </p>
+          </div>
+        </article>
+        <article className="gomunch__bottom">
+          <div className="gomunch__bottom-left">
+            <h2 className="values__title">
+              Interactúa con tu persona especial.
+            </h2>
+            <p>
+              {" "}
+              Recibe conﬁrmación visual del momento especial en que tu persona
+              cercana recibe lo que le enviaste.
+            </p>
+          </div>
+          <div className="gomunch__bottom-right">
+            <img
+              src={process.env.PUBLIC_URL + "/assets/img/mock3.png"}
+              alt="Relación a distancia"
+            ></img>
+          </div>
+        </article>
       </section>
-      <h1 className="importante">Mariana</h1>
-      <h2>Te quiero</h2>
-      <h3>motones</h3>
+      <section className="newsletter">
+        <article  className="newsletter__box">
+          <h2 className="values__title">
+            Incríbete ahora y recibe tu primer cupón
+          </h2>
+          <p>
+            Manten cercanía en tu relación a distancia, ¡inicia ahora y obten tu
+            cupón de descuento!
+          </p>
+          <div className="banner__text-input">
+            <p>Prueba goMunch gratis.</p>
+            <input type="text" className="inputText" placeholder="Correo electrónico"></input>
+            <button className="button">Probar ahora</button>
+          </div>
+        </article>
+      </section>
+      <div className="marca" >
+<p>Made by</p>
+      <img
+              src={process.env.PUBLIC_URL + "/assets/svg/Marcasola.svg"}
+              alt="Relación a distancia"
+            ></img>
+            </div>
     </div>
   );
 }

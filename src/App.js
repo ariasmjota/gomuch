ffb748b4b4a236624683c1c3f45673f5e9c6f0e5
Maria Arias <majobel10@hotmail.com>
2020-10-22 07:32:31 -0500

    do it 80

--- src/App.js
+++ src/App.js
@@ -16,49 +16,78 @@ function App() {
         </section>
       </nav>
       <section className="banner">
-        <article  className="banner__text">
+        <article className="banner__text">
           <h1>La distancia no es el fin.</h1>
           <p>
             GoMunch te permite compartir experiencias con esa persona que amas,
             pero que está lejos.
           </p>
-          <div  className="banner__text-input">
+          <div className="banner__text-input">
             <p>Prueba goMunch gratis.</p>
             <input type="text" className="inputText"></input>
             <button className="button">Probar ahora</button>
           </div>
         </article>
         <article className="banner__imagen">
-          <img className="banner__ima"
+          <img
+            className="banner__ima"
             src={process.env.PUBLIC_URL + "/assets/img/imagen.png"}
-            alt="Relación a distancia">
-          </img>
+            alt="Relación a distancia"
+          ></img>
         </article>
       </section>
       <section className="values">
         <h2 className="values__title">Con goMunch puedes</h2>
-        <article className="values__box">
+        <section className="values__box">
           <div className="values__box-one">
-            <img></img>
+            <img
+              src={process.env.PUBLIC_URL + "/assets/svg/amor.svg"}
+              alt="Relación a distancia"
+            ></img>
             <h3>Mantener cercanía con las personas a un mar de distancia. </h3>
           </div>
           <div className="values__box-two">
-            <img></img>
+            <img
+              src={process.env.PUBLIC_URL + "/assets/svg/relacion.svg"}
+              alt="Relación a distancia"
+            ></img>
             <h3>Disfrutar de momentos unicos y especiales con tu persona.</h3>
           </div>
-          <div className="values__three">
-            <img></img>
+          <div className="values__box-three">
+            <img
+              src={process.env.PUBLIC_URL + "/assets/svg/virtual.svg"}
+              alt="Relación a distancia"
+            ></img>
             <h3>Una experiencia que se adapta a tus necesidades.</h3>
           </div>
-        </article>
+        </section>
+        <section className="values__what">
+          <article className="values__what-left">
+            <h2 className="values__title">¿Qué es goMunch?</h2>
+            <p>
+              goMunch es una aplicación preocupada por la emotividad al momento
+              de compartir una comida entre personas separadas geográficamente.
+            </p>
+            <p>
+              Permite visualizar y mantener recuerdos de los diferentes momentos
+              que has compartido con tus personas cercanas.
+            </p>
+          </article>
+          <article className="values__what-right">
+            <img
+              src={process.env.PUBLIC_URL + "/assets/img/mock.png"}
+              alt="Relación a distancia"
+            ></img>
+          </article>
+          <div id="background"></div>
+        </section>
       </section>
-    <h1 className="importante">Mariana</h1>
-    <h2>
-      Te quiero
-    </h2>
-    <h3>
-      motones
-    </h3>
+      <section className="gomunch">
+
+      </section>
+      <h1 className="importante">Mariana</h1>
+      <h2>Te quiero</h2>
+      <h3>motones</h3>
     </div>
   );
 }

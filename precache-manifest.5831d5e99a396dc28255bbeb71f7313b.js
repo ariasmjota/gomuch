3bcc033da79750c77a2cf8d4cab772ac4849afcb
Maria Arias <majobel10@hotmail.com>
2020-10-22 07:37:11 -0500

    Updates

---
+++ precache-manifest.5831d5e99a396dc28255bbeb71f7313b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e664188ea5d0daa09208a9f6bd67ef",
+    "url": "/gomuch/index.html"
+  },
+  {
+    "revision": "2e8c344296682c338423",
+    "url": "/gomuch/static/css/main.6f1491aa.chunk.css"
+  },
+  {
+    "revision": "cd366e321cf17e9882bd",
+    "url": "/gomuch/static/js/2.e06cbd21.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/gomuch/static/js/2.e06cbd21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e8c344296682c338423",
+    "url": "/gomuch/static/js/main.2560dc16.chunk.js"
+  },
+  {
+    "revision": "0d7c819c1d66c24dd78c",
+    "url": "/gomuch/static/js/runtime-main.67d8117b.js"
+  }
+]);
